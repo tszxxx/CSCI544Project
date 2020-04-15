8a44f46f242aa65b37639f5b7203a13cbbeb3f67
郑杭捷 <zhenghangjie@126.com>
2020-04-15 09:33:25 -0700

    add features

--- CSCI544Project/CSCI544Project_fetchmp4.py
+++ CSCI544Project/CSCI544Project_fetchmp4.py
@@ -2,8 +2,11 @@ import os
 import requests
 import csv
 
-def fetchMP4(input_file_path, output_file_dir):
+def fetchMP4(input_file_path, output_file_dir, special_list=None):
     output_file_dir += '/'
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
+    }
     with open(input_file_path, 'r', encoding='UTF-8') as input_file:
         spamreader = csv.reader(input_file, delimiter=',', quotechar='\"')
         cnt = 0
@@ -12,11 +15,14 @@ def fetchMP4(input_file_path, output_file_dir):
                 if len(record[-1]) > 0:
                     url = record[-1]
                     id = record[0]
-                    response = requests.get(url)
-                    output_file = open(output_file_dir + id + '.mp4', 'wb')
-                    output_file.write(response.content)
-                    output_file.close()
-                print(cnt)
+                    if special_list is None or id in special_list:
+                        response = requests.get(url, headers=header)
+                        output_file = open(output_file_dir + id + '.mp4', 'wb')
+                        if len(response.content) == 0:
+                            print(id)
+                        output_file.write(response.content)
+                        output_file.close()
+                print(cnt, end='\r')
             cnt += 1
 
 if __name__ == '__main__':
@@ -24,5 +30,5 @@ if __name__ == '__main__':
     output_file_dir = '../Audio_Data'
     if not os.path.exists(output_file_dir):
         os.mkdir(output_file_dir)
-    fetchMP4(input_file_path, output_file_dir)
+    fetchMP4(input_file_path, output_file_dir, None) #['1947335'])
 
