dc7b26dd5ba51fbb64cbcfb658bcc13f2a47e8c4
Ding Li <dingli@Dings-MacBook-Pro.local>
2020-04-15 19:59:02 -0700

    Audio NN

--- CSCI544Project/CSCI544Project_fetchmp4.py
+++ CSCI544Project/CSCI544Project_fetchmp4.py
@@ -4,17 +4,21 @@ import csv
 
 def fetchMP4(input_file_path, output_file_dir, special_list=None):
     output_file_dir += '/'
+
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
     }
+
     with open(input_file_path, 'r', encoding='UTF-8') as input_file:
         spamreader = csv.reader(input_file, delimiter=',', quotechar='\"')
         cnt = 0
+        count = 0
         for record in spamreader:
             if cnt > 0:
                 if len(record[-1]) > 0:
                     url = record[-1]
                     id = record[0]
+
                     if special_list is None or id in special_list:
                         response = requests.get(url, headers=header)
                         output_file = open(output_file_dir + id + '.mp4', 'wb')
@@ -23,12 +27,15 @@ def fetchMP4(input_file_path, output_file_dir, special_list=None):
                         output_file.write(response.content)
                         output_file.close()
                 print(cnt, end='\r')
+
             cnt += 1
+        return count
 
 if __name__ == '__main__':
     input_file_path = '../Movie_information.csv'
     output_file_dir = '../Audio_Data'
     if not os.path.exists(output_file_dir):
         os.mkdir(output_file_dir)
+
     fetchMP4(input_file_path, output_file_dir, None) #['1947335'])
 
