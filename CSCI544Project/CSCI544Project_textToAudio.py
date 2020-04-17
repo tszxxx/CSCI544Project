cb1c80d75fc292527f855f3388dde5727f15aadc
Hangjie <hangjiez@usc.edu>
2020-04-17 00:28:28 -0700

    add features

--- CSCI544Project/CSCI544Project_textToAudio.py
+++ CSCI544Project/CSCI544Project_textToAudio.py
@@ -1,20 +1,32 @@
 import csv
 import os
-def get_text_to_audio(input_file_path, output_file_path):
+
+def get_text_to_audio(audio_file_path, input_file_path, output_file_path):
+    audio_dict = {}
     with open(input_file_path, 'r', encoding='utf-8') as input_file:
+        spamreader = csv.reader(input_file, delimiter=',', quotechar='\"')
+        cnt = 0
+        for record in spamreader:
+            if cnt > 0:
+                id = record[0]
+                score = record[6]
+                a, b = divmod(cnt, 4)
+                audio_dict[id] = {'score': score, 'index': b}
+            cnt += 1
+    with open(audio_file_path, 'r') as audio_file:
         with open(output_file_path, 'w') as output_file:
             cnt = 0
             segmentations_dirs = ['char_data', 'jieba_data', 'pku_data', 'snlp_data']
-            spamreader = csv.reader(input_file, delimiter=',', quotechar='\"')
+            spamreader = csv.reader(audio_file, delimiter=',', quotechar='\"')
             output_file.write('id,audio_path,char_path,jieba_path,pku_path,snlp_path\n')
             for record in spamreader:
                 if cnt > 0:
-                    id = record[0]
-                    score = record[6]
-                    a, b = divmod(cnt, 4)
-                    output_file.write(id + ',' + 'Audio_Data/' + id + '.mp4')
+                    id = record[0].split('.')[0]
+                    score = audio_dict[id]['score']
+                    index = audio_dict[id]['index']
+                    output_file.write(id + ',' + 'Audio_Data/' + record[0])
                     for root_dir in segmentations_dirs:
-                        file_path = root_dir + '/' + str(b) + '/' + score + '-' + id + '.txt'
+                        file_path = root_dir + '/' + str(index) + '/' + score + '-' + id + '.txt'
                         output_file.write(',' + file_path)
                     output_file.write('\n')
                     print(cnt)
@@ -22,6 +34,7 @@ def get_text_to_audio(input_file_path, output_file_path):
 
 
 if __name__ == '__main__':
+    audio_file_path = '../Audio_Data.csv'
     input_file_path = '../Movie_information.csv'
     output_file_path = '../Text_To_Audio.csv'
-    get_text_to_audio(input_file_path, output_file_path)
+    get_text_to_audio(audio_file_path, input_file_path, output_file_path)
